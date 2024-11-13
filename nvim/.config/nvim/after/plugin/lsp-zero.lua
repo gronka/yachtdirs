47d3e765cd33944e9d8429e944c608b3c3658579
tbg <taylor@gronka.us>
2024-11-13 01:33:56 -0600

    tweaks

--- nvim/.config/nvim/after/plugin/lsp-zero.lua
+++ nvim/.config/nvim/after/plugin/lsp-zero.lua
@@ -1,5 +1,20 @@
 vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]]
 
+vim.api.nvim_create_autocmd('CursorHold', {
+	buffer = bufnr,
+	callback = function()
+		local opts = {
+			focusable = false,
+			close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+			--border = 'rounded',
+			source = 'always',
+			prefix = ' ',
+			scope = 'cursor',
+		}
+		vim.diagnostic.open_float(nil, opts)
+	end
+})
+
 -- local zero = require('lsp-zero')
 
 -- require 'lspconfig'.dartls.setup({
